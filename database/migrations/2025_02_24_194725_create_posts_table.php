573f6198cf0d59ffd8270ee33b1c86056c55bca0
Nikita Osipovs <nikiton.osipoff@gmail.com>
2025-02-27 18:21:18 +0200

    some migration testings

--- database/migrations/2025_02_24_194725_create_posts_table.php
+++ database/migrations/2025_02_24_194725_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('rating')->default(0);
             $table->timestamps();
         });
     }
