9d97f85e77e25a37dcd1b6aaa86635fd1afc8fec
Nikita Osipovs <nikiton.osipoff@gmail.com>
2025-03-09 18:14:30 +0200

    Adding Location factory and seeding it

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Location;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory(10)->create();
+//        User::factory(10)->create();
+        Location::query()->delete();
+        Location::factory(10)->create();
+
+//        Location::->create([
+//            'title' => fake()->sentence(),
+//            'lat' => fake()->randomFloat(15,55.8, 58.1),
+//            'lng' => fake()->randomFloat(15,20.9, 28.3)
+//        ]); without factory
     }
 }
