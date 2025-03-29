8d9c7f1bb2567b9aca3b7f3ac7c6015f872b85f3
Nikita Osipovs <nikiton.osipoff@gmail.com>
2025-03-29 20:54:46 +0200

    Cleared flashMessage.vue and altered it visuals

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { ref, computed, onMounted, onUnmounted, } from 'vue';
-import { usePage } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
 
 const props = defineProps({
     flash: {
@@ -10,12 +9,6 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['close']);
-
-const visible = ref(true);
-
-const typeClass = computed(() => {
-    return props.flash.type === 'success' ? 'success' : 'error';
-});
 
 const close = () => {
     // visible.value = false;
@@ -29,41 +22,11 @@ onMounted(() => {
 </script>
 
 <template>
-    <div :class="['flash-message', typeClass, { 'flash-message--visible': visible }]">
+    <div :class="['flash-message', flash.type]">
         <span>{{ flash.message }}</span>
         <button @click="close">X</button>
     </div>
 </template>
-
-<!--<script>-->
-<!--export default {-->
-<!--    props: {-->
-<!--        flash: {-->
-<!--            type: Object,-->
-<!--            required: true,-->
-<!--        },-->
-<!--    },-->
-<!--    data() {-->
-<!--        return {-->
-<!--            visible: true,-->
-<!--        };-->
-<!--    },-->
-<!--    computed: {-->
-<!--        typeClass() {-->
-<!--            return this.flash.type === 'success' ? 'success' : 'error';-->
-<!--        },-->
-<!--    },-->
-<!--    mounted() {-->
-<!--        // console.log('message', this.message);-->
-<!--        setTimeout(this.close, 5000); // Auto close after 5 seconds-->
-<!--    },-->
-<!--    methods: {-->
-<!--        close() {-->
-<!--            this.visible = false;-->
-<!--        },-->
-<!--    }-->
-<!--};-->
-<!--</script>-->
 
 <style scoped>
 .flash-message {
@@ -73,21 +36,17 @@ onMounted(() => {
     padding: 10px 20px;
     border-radius: 5px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    opacity: 0;
     transition: opacity 0.3s ease;
     z-index: 1000;
+    background-color: #0056b3;
 }
 
-.flash-message--visible {
-    opacity: 1;
-}
-
-.flash-message--success {
+.success {
     background-color: #d4edda;
     color: #155724;
 }
 
-.flash-message--error {
+.error {
     background-color: #f8d7da;
     color: #721c24;
 }
